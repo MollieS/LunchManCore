69520f9d710e75f146f85b7eeee53ff9219e1948
Mollie Stephenson <mollie@8thlight.com>
2016-06-07 16:04:59 +0100

    Friday lunch can have an apprentice assigned to it

--- src/main/java/LunchManCore/FridayLunch.java
+++ src/main/java/LunchManCore/FridayLunch.java
@@ -1,10 +1,12 @@
 package LunchManCore;
 
 import java.util.Date;
+import java.util.Optional;
 
 public class FridayLunch {
 
     private final Date date;
+    private Apprentice apprentice;
 
     public FridayLunch(Date date) {
         this.date = date;
@@ -12,5 +14,16 @@ public class FridayLunch {
 
     public Date getDate() {
         return date;
+    }
+
+    public void assignApprentice(Apprentice apprentice) {
+        this.apprentice = apprentice;
+    }
+
+    public Optional<Apprentice> getApprentice() {
+        if (this.apprentice != null) {
+            return Optional.of(this.apprentice);
+        }
+        return Optional.empty();
     }
 }
--- src/test/java/LunchManCore/FridayLunchTest.java
+++ src/test/java/LunchManCore/FridayLunchTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,5 +14,13 @@ public class FridayLunchTest {
     public void hasADate() {
         FridayLunch fridayLunch = new FridayLunch(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
         assertEquals(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()), fridayLunch.getDate());
+    }
+
+    @Test
+    public void hasAnApprentice() {
+        FridayLunch fridayLunch = new FridayLunch(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
+        Apprentice nick = new Apprentice("Nick");
+        fridayLunch.assignApprentice(nick);
+        assertEquals(nick, fridayLunch.getApprentice().get());
     }
 }
