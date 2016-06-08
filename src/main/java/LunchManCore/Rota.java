d251d3aa2e10b6adfaca726d725d1680c589c3cc
Mollie Stephenson <mollie@8thlight.com>
2016-06-08 14:38:15 +0100

    Rewrite of rota class avoiding ConcurrentModificationException

--- src/main/java/LunchManCore/Rota.java
+++ src/main/java/LunchManCore/Rota.java
@@ -3,59 +3,93 @@ package LunchManCore;
 import java.util.*;
 
 import static java.util.Calendar.DAY_OF_WEEK;
+import static java.util.Calendar.FRIDAY;
 
 public class Rota {
 
     private final int scheduleCapacity;
-    private final Date startFromDate;
     private List<FridayLunch> schedule = new ArrayList<>();
 
-    public Rota(int scheduleCapacity, Date startFromDate) {
+    public Rota(int scheduleCapacity) {
         this.scheduleCapacity = scheduleCapacity;
-        this.startFromDate = startFromDate;
     }
 
     public List<FridayLunch> getSchedule() {
         return schedule;
     }
 
-    public void assign(FridayLunch nextFriday, Apprentice ced) {
-        nextFriday.assignApprentice(ced);
+    public void assign(FridayLunch nextFriday, Apprentice apprentice) {
+        nextFriday.assignApprentice(apprentice);
         schedule.add(nextFriday);
     }
 
-    public void updateSchedule(List<Apprentice> apprentices) {
-        for (FridayLunch friday : createFridays()) {
+    public void updateSchedule(List<Apprentice> apprentices, Date dateymcdateface) {
+        for (FridayLunch friday : createFridays(dateymcdateface)) {
             assign(friday, apprentices.get(0));
         }
     }
 
-    private List<FridayLunch> createFridays() {
-        List<FridayLunch> tmpList = new ArrayList<>();
-        for (int i = getSchedule().size(); i < scheduleCapacity; i++) {
-            tmpList.add(new FridayLunch(findNextFriday()));
-        }
-        return tmpList;
-    }
-
-    private Date findNextFriday() {
-        Date lastFriday;
-        if (schedule.size() > 0) {
-            lastFriday = schedule.get(schedule.size() - 1).getDate();
-        } else {
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(startFromDate);
-            if (calendar.get(DAY_OF_WEEK) == Calendar.FRIDAY) {
-                lastFriday = calendar.getTime();
-            } else {
-                calendar.set(DAY_OF_WEEK, Calendar.FRIDAY);
-                calendar.add(Calendar.DAY_OF_MONTH, -7);
-                lastFriday = calendar.getTime();
-            }
-        }
+    public Date findNextFriday(Date date) {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(lastFriday);
-        calendar.add(Calendar.DAY_OF_MONTH, 7);
+        calendar.setTime(date);
+        int diff = Calendar.FRIDAY - calendar.get(DAY_OF_WEEK);
+        if (!(diff > 0)) {
+            diff += 7;
+        }
+        calendar.add(Calendar.DAY_OF_MONTH, diff);
         return calendar.getTime();
     }
+
+    public List<Date> findNextFridays(Date date) {
+        List<Date> dates = new ArrayList<>();
+        for (int i = getSchedule().size(); i < scheduleCapacity; i++) {
+            dates.add(findNextFriday(date));
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            calendar.add(Calendar.DAY_OF_MONTH, 7);
+            date = calendar.getTime();
+        }
+        return dates;
+    }
+
+    public List<FridayLunch> createFridays(Date startDate) {
+        List<FridayLunch> lunches = new ArrayList<>();
+        if (!(schedule.isEmpty())) {
+            startDate = schedule.get(schedule.size() -1).getDate();
+        }
+        for (Date date : findNextFridays(startDate)) {
+            lunches.add(new FridayLunch(date));
+        }
+        return lunches;
+    }
+//
+//    private List<FridayLunch> createFridays(int scheduleSize) {
+//        List<FridayLunch> tmpList = new ArrayList<>();
+//        for (int i = scheduleSize; i < scheduleCapacity; i++) {
+//            tmpList.add(new FridayLunch(findNextFriday()));
+//            schedule.add(new FridayLunch(findNextFriday()))
+//        }
+//        return tmpList;
+//    }
+//
+//    private Date findNextFriday() {
+//        Date lastFriday;
+//        if (schedule.size() > 0) {
+//            lastFriday = schedule.get(schedule.size() - 1).getDate();
+//        } else {
+//            Calendar calendar = Calendar.getInstance();
+//            calendar.setTime(startFromDate);
+//            if (calendar.get(DAY_OF_WEEK) == Calendar.FRIDAY) {
+//                lastFriday = calendar.getTime();
+//            } else {
+//                calendar.set(DAY_OF_WEEK, Calendar.FRIDAY);
+//                calendar.add(Calendar.DAY_OF_MONTH, -7);
+//                lastFriday = calendar.getTime();
+//            }
+//        }
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(lastFriday);
+//        calendar.add(Calendar.DAY_OF_MONTH, 7);
+//        return calendar.getTime();
+//    }
 }
--- src/test/java/LunchManCore/RotaTest.java
+++ src/test/java/LunchManCore/RotaTest.java
@@ -1,5 +1,7 @@
 package LunchManCore;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.Timestamp;
@@ -11,53 +13,113 @@ import static junit.framework.TestCase.assertEquals;
 
 public class RotaTest {
 
+    private Rota rota;
+
+    @Before
+    public void setUp() {
+        this.rota = new Rota(4);
+    }
+
     @Test
     public void canAssignOneApprenticeToAFridayLunch() {
-        Rota rota = new Rota(5, new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
         FridayLunch nextFriday = new FridayLunch(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
         Apprentice ced = new Apprentice("Cedric");
         rota.assign(nextFriday, ced);
         assertEquals(ced, rota.getSchedule().get(0).getApprentice().get());
     }
 
+
     @Test
-    public void canAssignOneApprenticeToFourFridayLunches() {
-        Rota rota = new Rota(5, new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
-        Apprentice priya = new Apprentice("Priya");
-        List<Apprentice> apprentices = Arrays.asList(priya);
-        rota.updateSchedule(apprentices);
-        for (FridayLunch fridayLunch : rota.getSchedule()) {
-            assertEquals(priya, fridayLunch.getApprentice().get());
-        }
+    public void findsDateOfNextFridayWhenScheduleIsEmpty() {
+        Date date = new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime());
+        assertEquals(date, rota.findNextFriday(new Date(Timestamp.valueOf("2016-06-11 07:00:00").getTime())));
     }
 
     @Test
+    public void findsTheNextFourFridaysWhenScheduleIsEmpty() {
+        List<Date> result = rota.findNextFridays(new Date(Timestamp.valueOf("2016-06-11 07:00:00").getTime()));
+        assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), result.get(0));
+        assertEquals(new Date(Timestamp.valueOf("2016-06-24 07:00:00").getTime()), result.get(1));
+        assertEquals(new Date(Timestamp.valueOf("2016-07-01 07:00:00").getTime()), result.get(2));
+        assertEquals(new Date(Timestamp.valueOf("2016-07-08 07:00:00").getTime()), result.get(3));
+    }
+
+    @Test
+    public void createsTheNextFourFridayLunchesWhenScheduleIsEmpty() {
+        List<FridayLunch> result = rota.createFridays(new Date(Timestamp.valueOf("2016-06-11 07:00:00").getTime()));
+        assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), result.get(0).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-06-24 07:00:00").getTime()), result.get(1).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-01 07:00:00").getTime()), result.get(2).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-08 07:00:00").getTime()), result.get(3).getDate());
+    }
+
+    @Test
+    public void createsTheNext3FridayLunchesWhenScheduleHasOneFriday() {
+        FridayLunch nextFriday = new FridayLunch(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()));
+        Apprentice ced = new Apprentice("Cedric");
+        rota.assign(nextFriday, ced);
+        List<FridayLunch> result = rota.createFridays(new Date(Timestamp.valueOf("2016-06-15 07:00:00").getTime()));
+        assertEquals(new Date(Timestamp.valueOf("2016-06-24 07:00:00").getTime()), result.get(0).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-01 07:00:00").getTime()), result.get(1).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-08 07:00:00").getTime()), result.get(2).getDate());
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    public void canAssignOneApprenticeToFourFridayLunches() {
+        Apprentice priya = new Apprentice("Priya");
+        List<Apprentice> apprentices = Arrays.asList(priya);
+        rota.updateSchedule(apprentices, new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()));
+        List<FridayLunch> result = rota.getSchedule();
+        assertEquals(priya, result.get(0).getApprentice().get());
+        assertEquals(priya, result.get(1).getApprentice().get());
+        assertEquals(priya, result.get(2).getApprentice().get());
+        assertEquals(priya, result.get(3).getApprentice().get());
+    }
+
+    @Ignore
+    @Test
     public void knowsWhenTheNextFridayIsWhenScheduleHasData() {
-        Rota rota = new Rota(5, new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
+        Rota rota = new Rota(5);
         FridayLunch nextFriday = new FridayLunch(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
         Apprentice ced = new Apprentice("Cedric");
         rota.assign(nextFriday, ced);
         Apprentice priya = new Apprentice("Priya");
         List<Apprentice> apprentices = Arrays.asList(priya);
-        rota.updateSchedule(apprentices);
+//        rota.updateSchedule(apprentices);
         assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), rota.getSchedule().get(1).getDate());
     }
 
+    @Ignore
     @Test
     public void knowsWhenTheNextFridayIsWithEmptySchedule() {
-        Rota rota = new Rota(5, new Date(Timestamp.valueOf("2016-06-09 07:00:00").getTime()));
+        Rota rota = new Rota(5);
         Apprentice priya = new Apprentice("Priya");
         List<Apprentice> apprentices = Arrays.asList(priya);
-        rota.updateSchedule(apprentices);
-        assertEquals(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()), rota.getSchedule().get(1).getDate());
+//        rota.updateSchedule(apprentices);
+        assertEquals(new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()), rota.getSchedule().get(0).getDate());
     }
 
+    @Ignore
     @Test
     public void knowsWhenTheNextFridayFromAFriday() {
-        Rota rota = new Rota(5, new Date(Timestamp.valueOf("2016-06-10 07:00:00").getTime()));
+        Rota rota = new Rota(5);
         Apprentice priya = new Apprentice("Priya");
         List<Apprentice> apprentices = Arrays.asList(priya);
-        rota.updateSchedule(apprentices);
-        assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), rota.getSchedule().get(1).getDate());
+//        rota.updateSchedule(apprentices);
+        assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), rota.getSchedule().get(0).getDate());
+    }
+
+    @Ignore
+    @Test
+    public void calculatesAllFridaysForEmptySchedule() {
+        Rota rota = new Rota(5);
+        Apprentice priya = new Apprentice("Priya");
+        List<Apprentice> apprentices = Arrays.asList(priya);
+//        rota.updateSchedule(apprentices);
+        assertEquals(new Date(Timestamp.valueOf("2016-06-17 07:00:00").getTime()), rota.getSchedule().get(0).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-06-24 07:00:00").getTime()), rota.getSchedule().get(1).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-01 07:00:00").getTime()), rota.getSchedule().get(2).getDate());
+        assertEquals(new Date(Timestamp.valueOf("2016-07-08 07:00:00").getTime()), rota.getSchedule().get(3).getDate());
     }
 }
